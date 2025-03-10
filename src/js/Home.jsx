0141a680eef3fde02193f857788dceebfdd5370f
PabloPh95 <pablowork95@gmail.com>
2025-03-10 14:27:20 +0000

    TodoList_Actualizado

--- src/js/Home.jsx
+++ src/js/Home.jsx
@@ -14,14 +14,23 @@ function Home() {
 		setTasks([...tasks, newTask])
 	}
 
-	function removeTask(id){
-		setTasks(tasks.filter(task => task.id !== id))
-	} // Función para eliminar las tareas, se enlaza a TaksList y luego a ItemList
+	function removeTask(id) {
+		setTasks(prevTasks => prevTasks.filter(task => task.id !== id));
+	}
+ // Función para eliminar las tareas, se enlaza a TaksList y luego a ItemList
+
+	function removeAllWithForEach() {
+		tasks.forEach((task) => {;
+			removeTask(task.id);
+		});
+	}
+	// Función para eliminar todas la tareas
 
 	return(
 		<>
 			<div className="container mt-5">
 				< TaksForm onAddTask={addTasks} /> {/*  <Input> para escribir las tareas */}
+				<button onClick={removeAllWithForEach}>Borrar Todo</button> {/*  BUTTON PARA BORRAR TODO (PARA OTRO PROYECTO) */}
 				< TaksList tasks={tasks} onDelete={removeTask} /> {/* Zona donde se imprime los datos capturados del <Input> */}
 			</div>
 		</>
